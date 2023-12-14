c343968a8c89cba89d333aa01ecce6f52e11494f
Chris Burton <burtfest@gmail.com>
2023-12-13 18:07:06 -0700

    tweaks

--- samples/augment_sys_path.py
+++ samples/augment_sys_path.py
@@ -7,12 +7,15 @@ import glob, sys, os
 #   augment_sys_path(wd, True)
 #
 def augment_sys_path(path, recursive = False):
-    if not path in set(sys.path) and os.path.isdir(path) and os.path.exists(path):
+    current_paths = tuple(set(filter(lambda x :not x == '', sys.path)))
+
+    if (not path in current_paths and not path.rstrip('/') in current_paths and os.path.isdir(path) and os.path.exists(path)):
         sys.path.append(path)
 
     if recursive:
         for filename in glob.iglob(path, recursive=True):
-            if os.path.isdir(filename) and os.path.exists(filename) and not filename in sys.path: 
+            if (os.path.isdir(filename) and os.path.exists(filename) 
+                    and not (filename in current_paths or filename.rstrip('/') in current_paths) and not os.path.basename(filename).startswith(tuple(["_","."]))):
                 sys.path.append(filename)
 
     return sys.path
