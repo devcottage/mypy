40742d508886c68e097e51738bd62d839e349353
Chris Burton <burtfest@gmail.com>
2023-12-20 10:27:01 -0700

    passphrase tweak

--- tools/passphrase.py
+++ tools/passphrase.py
@@ -4,11 +4,12 @@ import string
 
 help = sys.argv[0] + " --seperator <word-seperator>  <word-count>"
 
-punctuation = ['_', '=', '+', '--']
+punctuation = ['_', '=', '+', '--', '.', ',']
 
 tool_dir = os.path.dirname(os.path.abspath(__file__))
 all_words = tuple(open(os.path.join(tool_dir, "wordlist.txt"), 'r'))
 phrase_words = list(map(lambda w: w.strip(), list(filter(lambda w: len(w) > 4 and len(w) < 13, all_words))))
+print(f"dictionary size={len(phrase_words)}", file=sys.stderr)
 
 # handle command-line options
 opts,args = getopt.getopt(sys.argv[1:], "hs:v", ["help", "seperator="])
@@ -25,7 +26,7 @@ except Exception as ex:
    exit()
 
 # use random.choice() for words in phrase, and add in a number sequence, usually required
-words = [random.choice(phrase_words) for _ in range(0,word_count - 1)]
+words = [random.choice(phrase_words) for _ in range(word_count)]
 words = list(map(lambda w : w.title().strip(), words))
 words.append(str(random.randint(1000,9999)))
 random.shuffle(words)
@@ -35,5 +36,5 @@ pass_phrase = words.pop()
 for w in words:
     pass_phrase += random.choice(punctuation) + w
 
-print(f"{word_count} word/{len(pass_phrase)} character phrase:\n", file=sys.stderr)
+print(f"{word_count} word(s) + 1 number / {len(pass_phrase)} character phrase, with punctuation:\n", file=sys.stderr)
 print(f"{pass_phrase}"); print("", file=sys.stderr)
