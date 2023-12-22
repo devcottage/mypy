8856bc2bede0eaffd510a1556abaa80e176f6532
Chris Burton <burtfest@gmail.com>
2023-12-22 10:45:00 -0700

    help msg

--- tools/passphrase.py
+++ tools/passphrase.py
@@ -2,7 +2,11 @@ import getopt, sys, os
 import random
 import string
 
-help = sys.argv[0] + " --with-nums <n> --seperator <word-seperator> --maxword <max-word-length> <word-count> "
+DEFAULT_WORD_COUNT=3
+help = (sys.argv[0] + " --with-nums <n> --seperator <word-seperator> --maxword <max-word-length> <word-count> \n" + 
+        "  example:  python3 passphrase.py -m 8 -s _ -w 2 3 \n"
+        "            (max word len = 8 characters, 3 random words, and 2 numbers, with an underscore seperator character) \n"
+        )
 
 punctuation = ['_', '=', '+', '--', '.', ',']
 
@@ -24,7 +28,7 @@ try:
     elif o in ("-w", "--with-nums"):
         num_count = int(v)
 
-  word_count = int(args[-1] if len(args) > 0 else 5)
+  word_count = int(args[-1] if len(args) > 0 else DEFAULT_WORD_COUNT)
 except Exception as ex: 
    print(help)
    exit()
@@ -37,7 +41,7 @@ print(f"dictionary size={len(phrase_words)}", file=sys.stderr)
 words = [random.choice(phrase_words) for _ in range(word_count)]
 words = list(map(lambda w : w.title().strip(), words))
 for c in range(num_count):
-    words.append(str(random.randint(1000,99999)))
+    words.append(str(random.randint(100,99999)))
 
 random.shuffle(words)
 
@@ -46,5 +50,5 @@ pass_phrase = (words if len(words) > 0 else [""]).pop()
 for w in words:
     pass_phrase += random.choice(punctuation) + w
 
-print(f"{word_count} word(s) + {num_count} number / {len(pass_phrase)} character phrase, with punctuation:\n", file=sys.stderr)
+print(f"{word_count} word(s) + {num_count} number(s) / {len(pass_phrase)} character phrase, with punctuation:\n", file=sys.stderr)
 print(f"{pass_phrase}"); print("", file=sys.stderr)
